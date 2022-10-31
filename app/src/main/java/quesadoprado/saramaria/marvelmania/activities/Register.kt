d237aa0a22c66d64e4a8be09d365ee0ed657bee4
Sara Maria Quesado Prado <saraquesadoprado@gmail.com>
2022-10-31 08:16:12 +0100

    empezando con el login

--- app/src/main/java/quesadoprado/saramaria/marvelmania/activities/Register.kt
+++ app/src/main/java/quesadoprado/saramaria/marvelmania/activities/Register.kt
@@ -1,12 +1,53 @@
 package quesadoprado.saramaria.marvelmania.activities
 
+import android.app.ProgressDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import quesadoprado.saramaria.marvelmania.R
+import quesadoprado.saramaria.marvelmania.databinding.ActivityRegisterBinding
 
 class Register : AppCompatActivity() {
+    lateinit var bindind:ActivityRegisterBinding
+    lateinit var auth:FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        bindind=ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(bindind.root)
+        auth=Firebase.auth
+        inicializar()
+    }
+    fun inicializar(){
+        val user=bindind.user.text.toString()
+        val email=bindind.email.text.toString()
+        val pass=bindind.password.text.toString()
+        val repeatpass=bindind.repeatpassword.text.toString()
+        val progressBar=ProgressDialog(this)
+        bindind.btnRegistrar.setOnClickListener {
+            if (bindind.user.text==null||bindind.email.text==null||bindind.password.text==null||bindind.repeatpassword.text==null){
+                Toast.makeText(this,getString(R.string.camposRellenos),Toast.LENGTH_SHORT).show()
+            }else{
+                if(comprobarcontraseña(pass,repeatpass)){
+                    progressBar.setMessage(getString(R.string.user_register))
+                    progressBar.show()
+                    auth.createUserWithEmailAndPassword(email, pass)
+                        .addOnCompleteListener(this){ task->
+                            if(task.isSuccessful) {
+                                val user: FirebaseUser = auth.currentUser!!
+                                Toast.makeText(this, user.toString(), Toast.LENGTH_SHORT).show()
+                            }else{
+                                Toast.makeText(this, "NO FURULA", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                }
+            }
+        }
+    }
+    fun comprobarcontraseña(pass: String, repeatpass: String):Boolean{
+        return pass == repeatpass
     }
 }
--- app/src/main/java/quesadoprado/saramaria/marvelmania/fragments/LoginFragment.kt
+++ app/src/main/java/quesadoprado/saramaria/marvelmania/fragments/LoginFragment.kt
@@ -1,5 +1,6 @@
 package quesadoprado.saramaria.marvelmania.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,10 +10,11 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
 import quesadoprado.saramaria.marvelmania.R
+import quesadoprado.saramaria.marvelmania.activities.Register
 
-class LoginFragment(private val auth: FirebaseAuth) : Fragment() {
-    var conectado=false
-    val currentUser = auth.currentUser
+class LoginFragment(private var auth: FirebaseAuth) : Fragment() {
+    private var conectado=false
+    private val currentUser = auth.currentUser
     override fun onStart() {
         super.onStart()
         if(currentUser != null){
@@ -23,7 +25,7 @@ class LoginFragment(private val auth: FirebaseAuth) : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        if (conectado==false){
+        if (!conectado){
             return inflater.inflate(R.layout.fragment_login,container,false)
         }else{
             return inflater.inflate(R.layout.login_conect,container,false)
@@ -32,10 +34,11 @@ class LoginFragment(private val auth: FirebaseAuth) : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        if (conectado==false) {
+        if (!conectado) {
             val registrar: TextView = view.findViewById(R.id.registrar)
             registrar.setOnClickListener {
-
+                val intentRegistro= Intent(context,Register::class.java)
+                startActivity(intentRegistro)
             }
         }else{
             Toast.makeText(context,"ESTO FUNCIONA2",Toast.LENGTH_SHORT).show()
